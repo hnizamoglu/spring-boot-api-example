34843e5f700ee3ca8e398af879400d0b5cf7e74c
Hakan Nizamoglu <hnizamoglu@gmail.com>
2020-06-24 16:26:09 +0200

    fix

--- src/main/java/com/tomgregory/controller/ThemeParkRideController.java
+++ src/main/java/com/tomgregory/controller/ThemeParkRideController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+/**
+    Some comments.
+ */
 
 @RestController
 public class ThemeParkRideController {
